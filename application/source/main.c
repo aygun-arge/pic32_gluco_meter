44ac7fcdb8a1add68b8bf63484485437d466f574
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-10-22 22:05:48 +0200

    Added application folder

---
+++ application/source/main.c
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.c
+ * Author: nenad
+ *
+ * Created on 22 October 2014, 21:35
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "driver/adc.h"
+#include "driver/clock.h"
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "driver/intr.h"
+#include "driver/rtc.h"
+#include "driver/s25fl.h"
+#include "driver/spi.h"
+#include "driver/systick.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
