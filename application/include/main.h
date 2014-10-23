e4bdf29e5986e23261409f2c561f909aa3634ed6
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-10-23 23:44:22 +0200

    Application include folder

---
+++ application/include/main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: nenad
+ *
+ * Created on 22 October 2014, 23:19
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+struct i2c_bus g_i2c1_bus;
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
