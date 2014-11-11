2d2c4f9ad42aeae31f5ebd216778be11a3043594
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-11-11 23:32:45 +0100

    Created VOC frequency measurement module

---
+++ application/source/voc_freq.c
@@ -0,0 +1,13 @@
+
+#include <xc.h>
+
+#define VOC_FREQ_GPIO                   LATF
+#define VOC_FREQ_TRIS                   TRISF
+#define VOC_FREQ_PIN                    5
+
+#define VOC_FREQ_PIN_TO_CN_NO(x)        
+
+void voc_freq_init(void)
+{
+    
+}
