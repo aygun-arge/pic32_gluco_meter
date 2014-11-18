1652ef217edbf995fb98e265dad6b2ed0e921522
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-11-18 19:19:57 +0100

    Further work

--- application/source/voc_freq.c
+++ application/source/voc_freq.c
@@ -8,7 +8,7 @@
 #include <peripheral/timer.h>
 #include "arch/intr.h"
 
-#define CONFIG_IC_SELECTION             1
+#define CONFIG_IC_SELECTION             4
 #define CONFIG_NUM_OF_SAMPLES           16
 
 #define ICxCON_ON                       (0x1u << 15)
@@ -22,7 +22,7 @@
 #define ICxCON_ICBNE                    (0x1u << 3)
 #define ICxCON_ICM_Pos                  (0)
 #define ICxCON_ICM_Msk                  (0x7u << ICxCON_ICM_Pos)
-#define ICxCON_ICM(x)                   (((x) << ICxCON_ICM_Pos) & ICxCON_ICM_Pos)
+#define ICxCON_ICM(x)                   (((x) << ICxCON_ICM_Pos) & ICxCON_ICM_Msk)
 
 #define ICxCON_EVERY_FOURTH             3
 #define ICxCON_EVERY_THIRD              2
@@ -74,6 +74,8 @@
 #define ICxIPC                          IPC4
 #define ICxISR_BIT                      17
 #define ICxPRIO_BIT                     8
+#define ICxTRIS                         TRISD
+#define ICxPIN                          11
 #else
 #define IC_VECTOR                       _INPUT_CAPTURE_5_VECTOR
 #define ICxCON                          IC5CON
@@ -85,32 +87,88 @@
 #define ICxPRIO_BIT                     8
 #endif
 
+#define TxCON_ON                        (0x1u << 15)
+#define T1CON_TWDIS                     (0x1u << 12)
+#define T1CON_TWIP                      (0x1u << 11)
+#define TxCON_TGATE                     (0x1u << 7)
+#define T1CON_TCKPS_Pos                 (4)
+#define T1CON_TCKPS_Msk                 (0x3u << T1CON_TCKPS_Pos)
+#define T1CON_TCKPS(x)                  (((x) << T1CON_TCKPS_Pos) & T1CON_TCKPS_Msk)
+#define TxCON_TCKPS_Pos                 (4)
+#define TxCON_TCKPS_Msk                 (0x7u << TxCON_TCKPS_Pos)
+#define TxCON_TCKPS(x)                  (((x) << TxCON_TCKPS_Pos) & TxCON_TCKPS_Msk)
+#define TxCON_T32                       (0x1u << 3)
+#define T1CON_TSYNC                     (0x1u << 2)
+#define TxCON_TCS                       (0x1u << 1)
 
 static uint32_t g_current_val;
+static bool     g_is_sampling;
 
 void __ISR(IC_VECTOR) change_notice_isr(void)
 {
-    uint32_t            first_val;
-    uint32_t
+    static uint32_t     no;
+    static uint64_t     acc;
+    uint32_t            first;
+    uint32_t            second;
+    uint32_t            current;
 
-    ICxIFS &= (0x1u << ICxISR_BIT);
+    first = ICxBUF;
+    second  = ICxBUF;
+
+    if (second > first) {
+        current = second - first;
+    } else {
+        current = UINT32_MAX - (first - second);
+    }
+    acc += (uint64_t)current;
+    no++;
+
+    if (no == CONFIG_NUM_OF_SAMPLES) {
+        no  = 0;
+        g_current_val = (uint32_t)(acc / (uint64_t)CONFIG_NUM_OF_SAMPLES);
+        g_is_sampling = false;
+        acc = (uint64_t)0ul;
+    }
+
+    while (ICxCON & ICxCON_ICBNE) {
+        volatile uint32_t dummy;
+
+        dummy = ICxBUF;
+    }
+    ICxIFS &= ~(0x1u << ICxISR_BIT);
 }
 
 void voc_freq_init(void)
 {
+    g_is_sampling = true;
+    g_current_val = 0u;
+    ICxTRIS |= (0x1u << ICxPIN);
     ICxCON  = 0;
+    T2CON   = 0;
+    T3CON   = 0;
     Delay10us(10);
+    T2CON   = TxCON_T32;
+    TMR2    = 0;
+    PR2     = UINT32_MAX;
     ICxCON  = ICxCON_FEDGE | ICxCON_C32 | ICxCON_ICI(ICxCON_EVERY_SECOND) |
               ICxCON_ICM(ICxCON_MODE_SIMPLE_FALLING);
-    ICxIEC &= (0x1u << ICxISR_BIT);
-    ICxIFS &= (0x1u << ICxISR_BIT);
-    ICxIPC &= (0x1fu << ICxPRIO_BIT);
+    ICxIEC &= ~(0x1u << ICxISR_BIT);
+    ICxIFS &= ~(0x1u << ICxISR_BIT);
+    ICxIPC &= ~(0x1fu << ICxPRIO_BIT);
     ICxIPC |= (CONFIG_INTR_MAX_ISR_PRIO << (ICxPRIO_BIT + 2u));
     ICxIEC |= (0x1u << ICxISR_BIT);
+    T2CON  |= TxCON_ON;
+    T2CON  |= TxCON_ON;
     ICxCON |= ICxCON_ON;
+}
+
+bool voc_is_sampling(void)
+{
+    return (g_is_sampling);
 }
 
 uint32_t voc_freq_raw(void)
 {
+    g_is_sampling = true;
     return (g_current_val);
 }
